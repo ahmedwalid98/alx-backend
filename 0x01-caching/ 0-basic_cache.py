8e32d0984e258898ca377c883542ae6e8df7c5d4
root <root@DESKTOP-4KC1C5P>
2024-01-31 21:22:29 +0200

    pagination

--- 0x01-caching/ 0-basic_cache.py
+++ 0x01-caching/ 0-basic_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""module"""
+"""Basic caching module.
+"""
 from base_caching import BaseCaching
 
 
@@ -8,11 +9,13 @@ class BasicCache(BaseCaching):
     retrieving items from a dictionary.
     """
     def put(self, key, item):
-        """docs"""
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
-        """docs"""
+        """Retrieves an item by key.
+        """
         return self.cache_data.get(key, None)
